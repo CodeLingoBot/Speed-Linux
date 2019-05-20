6abd817812786b9f71c2e851ab923695c8972b34
Karolis Gudas <zedanas123@gmail.com>
2019-05-20 11:06:36 +0100

    push

--- dialog.sh
+++ dialog.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#######################################################
-# btb installer
-#######################################################
+# ╭――╮
+# SPOS ─ text graphic interface.
+# ╰──╯	 SPOS - basic-info-graph.inc
+#	├──▶ Start required config.
+#	╰──▶ First server config file.
 source btb-libs/btbSC.inc
 source btb-libs/btbCC.inc
 PWD=pwd
@@ -25,47 +27,6 @@ elif [[ "$COS8" = '8' && ! -f /bin/wget ]]; then
 fi
 
 #######################################################
-source btb-libs/btbYum.inc # dnf installation
+source SpeedLIB/required-details-graph.inc # dnf installation
 #######################################################
-if [[ "$COS7" = '7' ]]; then
-    echo "Check for existing mariadb packages"
-    MARIADB=`rpm -qa | grep 'mariadb' | head -n1`
-    if [[ ! -z "$MARIADB" ]]; then
-        $RPR $MARIADB
-		cecho "Removed: $MARIADB" $red
-    fi
-	echo "Check for existing mariadb packages"
-    MARIADBSERVER=`rpm -qa | grep 'mariadb-server' | head -n1`
-    if [[ ! -z "$OLDMYSQLSERVER" ]]; then
-       $RPR $MARIADBSERVER
-	   cecho "Removed: $MARIADBSERVER" $red
-    fi
-    echo "Check for existing mariadb-libs package"
-    MARIADBLIBS=`rpm -qa | grep 'mariadb-libs' | head -n1`
-    if [[ ! -z "$MARIADBLIBS" ]]; then
-        $RPR $MARIADBLIBS
-		cecho "Removed: $MARIADBLIBS" $red
-    fi
-    echo "Check for existing MySQL-shared-compat"
-    SQLSHAREDCOMPAT=`rpm -qa | grep 'MySQL-shared-compat' | head -n1`
-    if [[ ! -z "$SQSHAREDCOMPAT" ]]; then
-		$RPR $SQSHAREDCOMPAT
-        cecho "Removed: $SQSHAREDCOMPAT" $red
-        
-    fi
-	echo "Check for existing MySQL-community-release"
-    MYSQL=`rpm -qa | grep 'mysql80-community-rellease' | head -n1`
-    if [[ ! -z "$MYSQL" ]]; then
-		$RPUQ $MYSQL
-		cecho "Updated: $MYSQL" $green
-	else
-		$RPI http://repo.mysql.com/yum/mysql-8.0-community/el/7/x86_64/mysql80-community-release-el7-3.noarch.rpm
-		$RPI  mysql-community-server mysql-community-client
-		MYSQLS=`rpm -qa | grep 'mysql-community-server' | head -n1`
-		MYSQLC=`rpm -qa | grep 'mysql-community-client' | head -n1`
-        cecho "Installed: $MYSQLC & " $green
-      
-    fi
-	
-fi
-echo $RPI;
+
